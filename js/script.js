e5b0ff3ae84a91bd1cb67691527bf5f8481f3043
Federico-Zorzi <federico.zorzi05@gmail.com>
2024-10-29 17:02:22 +0100

    add a member button

--- js/script.js
+++ js/script.js
@@ -37,36 +37,70 @@ const teamMembers = [
   },
 ];
 
+// * raccolgo gli elementi dal HTML
 const gridTeamEl = document.getElementById("grid-team");
 
-const printCard = () => {
-  let cardMemberHtml = ``;
+const fullnameEl = document.getElementById("card-fullname");
+const roleEl = document.getElementById("card-role");
+const emailEl = document.getElementById("card-email");
+const imageEl = document.getElementById("card-image");
+const addMemberEl = document.getElementById("add-member");
 
-  for (let member of teamMembers) {
-    let memberName = member.name;
-    let upperCaseMemberName = memberName.toUpperCase();
+const textUpperCase = (text) => {
+  let newText = text.toUpperCase();
+  return newText;
+};
 
-    cardMemberHtml += `
+const printCard = (name, role, email, image) => {
+  return `
     <div class="col">
       <div class="card">
         <div class="row g-0 text-light bg-black h-100">
           <div class="col-md-4">
-            <img src="./${member.img}" class="img-fluid" alt="${member.name} - ${member.role}" />
+            <img src="./${image}" class="img-fluid" alt="${name} - ${role}" />
           </div>
           <div class="col-md-8">
             <div class="card-body h-100">
-              <h5 class="fw-bold">${upperCaseMemberName}</h5>
-              <span>${member.role}</span>
+              <h5 class="fw-bold">${textUpperCase(name)}</h5>
+              <span>${role}</span>
               <address class="mb-0">
-                <a href="#" class="link-info">${member.email}</a>
+                <a href="#" class="link-info">${email}</a>
               </address>
             </div>
           </div>
         </div>
       </div>
     </div>`;
+};
+
+// * funzione aggiungi card
+const addCard = () => {
+  let cardMemberHtml = ``;
+
+  for (let member of teamMembers) {
+    cardMemberHtml += printCard(
+      member.name,
+      member.role,
+      member.email,
+      member.img
+    );
   }
   gridTeamEl.innerHTML = cardMemberHtml;
 };
 
-printCard();
+const addMember = () => {
+  let name = fullnameEl.value;
+  let role = roleEl.value;
+  let email = emailEl.value;
+  let img = imageEl.value;
+
+  return teamMembers.push({ name, role, email, img });
+};
+
+addCard();
+
+addMemberEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addMember();
+  addCard();
+});
